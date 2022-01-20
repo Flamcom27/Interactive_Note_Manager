a7e85166cd81fabfaf7d2970dd414a07313f4f0c
Flamcom27 <27flamcom06@gmail.com>
2022-01-20 23:02:29 +0300

    first commit

---
+++ Interactive_Note_Manager/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Interactive_Note_Manager project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Interactive_Note_Manager.settings')
+
+application = get_asgi_application()
